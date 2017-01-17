8ac49a872232679654a8b71daa4743f02d5503f4
salilgupta1 <salil.gupta323@gmail.com>
2017-01-16 16:16:41 -0800

    local environment vars for database

--- norm/settings/common.py
+++ norm/settings/common.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'norm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT']
+        'NAME': 'norm',
+        'USER': 'salilgupta',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
