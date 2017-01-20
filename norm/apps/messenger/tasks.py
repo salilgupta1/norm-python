85fe5bb7a8bfa005105e3f70c1faa9fd13e8437d
salilgupta1 <salil.gupta323@gmail.com>
2017-01-19 19:35:24 -0800

    putting in the full path for imported modules

--- norm/apps/messenger/tasks.py
+++ norm/apps/messenger/tasks.py
@@ -4,8 +4,8 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
-import controller
-import fb
+import norm.apps.messenger.controller as controller
+import norm.apps.messenger.fb as fb
 
 logger = get_task_logger(__name__)
 
